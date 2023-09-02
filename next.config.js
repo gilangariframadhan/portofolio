a94f4a3f365a5f654bb7bcefbed68b92f551e788
gilangariframadhan <55501794+gilangariframadhan@users.noreply.github.com>
2023-09-02 18:17:53 +0700

    nextConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["example.com"], // Ganti 'example.com' dengan domain dari mana Anda ingin memuat gambar.
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
