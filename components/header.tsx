51ffba36b8009a63d045e6475dd7db4d1ad66cd6
gilangariframadhan <55501794+gilangariframadhan@users.noreply.github.com>
2023-09-04 17:21:58 +0700

    nextCommit

--- components/header.tsx
+++ components/header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
     return (
         <header className="header">
             <div className='content'>
-                <Image src="/profile.jpg" alt="Profile" className="logo" width={100} height={100} />
+                <img src="/profile.jpg" alt="Profile" className="logo" width={100} height={100} />
                 <button className="hamburger" onClick={toggleMenu}>
                     &#9776;  {/* Unicode untuk icon hamburger */}
                 </button>
